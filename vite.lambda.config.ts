f2e3617ff44a0197231c14c253d0c88e2409a291
Gautier <gautier.blandin@gmail.com>
2024-06-02 20:59:57 +0200

    feat(init): add vite external dependencies

--- vite.lambda.config.ts
+++ vite.lambda.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  optimizeDeps: {
+    include: ['linked-dep'],
+  },
   build: {
     outDir: 'dist/lambda',
     lib: {
@@ -10,7 +13,10 @@ export default defineConfig({
       formats: ['cjs'],
     },
     rollupOptions: {
-      external: ['node:stream'],
+      external: ['node:stream', '@remix-run/architect', 'react-dom', 'react'],
+    },
+    commonjsOptions: {
+      include: [/linked-dep/, /node_modules/],
     },
   },
 });
