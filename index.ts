98cbbaa4231baea7e418bdf0b9b044c536f6d2c1
Gautier <gautier.blandin@gmail.com>
2024-06-02 11:54:34 +0200

    feat(init): add pulumi index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,35 @@
+import * as pulumi from '@pulumi/pulumi';
+import * as aws from '@pulumi/aws';
 import * as apigateway from '@pulumi/aws-apigateway';
-import { pulumiHandler } from './server';
+
+const assumeRole = aws.iam.getPolicyDocument({
+  statements: [
+    {
+      effect: 'Allow',
+      principals: [
+        {
+          type: 'Service',
+          identifiers: ['lambda.amazonaws.com'],
+        },
+      ],
+      actions: ['sts:AssumeRole'],
+    },
+  ],
+});
+
+const iamForLambda = new aws.iam.Role('iam_for_lambda', {
+  name: 'iam_for_lambda',
+  assumeRolePolicy: assumeRole.then((assumeRole) => assumeRole.json),
+});
+
+const lambda = new aws.lambda.Function('myLambda', {
+  code: new pulumi.asset.AssetArchive({
+    '.': new pulumi.asset.FileArchive('./dist/lambda'),
+  }),
+  handler: 'handler.handler',
+  runtime: aws.lambda.Runtime.NodeJS20dX,
+  role: iamForLambda.arn,
+});
 
 // Create an API Gateway REST API
 const remixApi = new apigateway.RestAPI('remix-api', {
@@ -7,7 +37,7 @@ const remixApi = new apigateway.RestAPI('remix-api', {
     {
       path: '/{proxy+}',
       method: 'ANY',
-      eventHandler: pulumiHandler,
+      eventHandler: lambda,
     },
   ],
 });
